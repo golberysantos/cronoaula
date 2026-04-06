42ba958bc894b6ab80f2be6ff4c9d0aafe411c7c
Golbery Santos <golbery.santos@yahoo.com.br>
2026-04-06 01:05:12 -0400

    Ativar o tema

--- src/main/java/br/com/cronoaula/appcronoaula/AppCronoAula.java
+++ src/main/java/br/com/cronoaula/appcronoaula/AppCronoAula.java
@@ -3,10 +3,16 @@ package br.com.cronoaula.appcronoaula;
 import br.com.cronoaula.cronograma.view.CronogramaView;
 
 import javax.swing.*;
-
+import com.formdev.flatlaf.FlatLightLaf;
 public class AppCronoAula {
 
     public static void main(String[] args) {
+    	
+    	try {
+            FlatLightLaf.setup();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         SwingUtilities.invokeLater(() -> {
             new CronogramaView().setVisible(true);
